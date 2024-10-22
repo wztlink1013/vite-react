1fc720ef397c79acce0de50b9266e495685270ff
wuzt <wzt@differsoft.com>
2024-10-22 17:01:29 +0800

    public folder

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,26 @@
 // 'use client';
 
+import Image from 'next/image';
 import { Button } from 'antd';
 
 export default function Home() {
   return (
     <div className="p-2 border border-solid border-yellow-200 rounded">
       <div>page page</div>
-      <div>
-        <Button type='primary'>antd button</Button>
+      <div className="flex flex-row justify-between">
+        <div>
+          public file:{' '}
+          <Image
+            aria-hidden
+            src="/file.svg"
+            alt="File icon"
+            width={16}
+            height={16}
+          />
+        </div>
+        <div>
+          <Button type="primary">antd button</Button>
+        </div>
       </div>
     </div>
   );
