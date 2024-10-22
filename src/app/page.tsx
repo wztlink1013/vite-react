1322bfb4c162f3546d7b21a41e6cf9aea9f3cf6e
wuzt <wzt@differsoft.com>
2024-10-22 16:51:29 +0800

    next14 antd tailwind

---
+++ src/app/page.tsx
@@ -0,0 +1,14 @@
+// 'use client';
+
+import { Button } from 'antd';
+
+export default function Home() {
+  return (
+    <div className="p-2 border border-solid border-yellow-200 rounded">
+      <div>page page</div>
+      <div>
+        <Button type='primary'>antd button</Button>
+      </div>
+    </div>
+  );
+}
